ce73ad9c73c69cc92ae02beb3de03a20a8baf551
sroopsai <roop.sai@outlook.in>
2022-03-18 03:01:36 +0100

    Add CheckPrime and CheckPrimeTest

---
+++ src/main/java/org/roopsai/Mathematics/CheckPrime.java
@@ -0,0 +1,12 @@
+package org.roopsai.Mathematics;
+
+public class CheckPrime {
+
+    public static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/org/roopsai/Mathematics/CheckPrimeTest.java
@@ -0,0 +1,12 @@
+package org.roopsai.Mathematics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckPrimeTest {
+
+    @Test
+    public void testIsPrime() {
+        Assert.assertTrue( CheckPrime.isPrime(101));
+    }
+}
